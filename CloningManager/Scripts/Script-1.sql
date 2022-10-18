51b3c6e0922588541971889adf71557581ee0343
Matěj Duháček <duhacek@develogi.cz>
2022-10-18 12:46:00 +0200

    dont know 2

--- CloningManager/Scripts/Script-1.sql
+++ CloningManager/Scripts/Script-1.sql
@@ -24,12 +24,21 @@
 -- DROP TABLE inventory.products;
 
 
+-- CREATE TABLE public.products (
+-- 	id serial4 NOT NULL,
+-- 	"name" varchar(255) NOT NULL,
+-- 	description varchar(512) NULL,
+-- 	weight float8 NULL,	
+-- 	CONSTRAINT products_pkey PRIMARY KEY (id)
+-- );
+
 CREATE TABLE public.products (
-	id serial4 NOT NULL,
-	"name" varchar(255) NOT NULL,
+	id NULL,
+	"name" varchar(255) NULL,
 	description varchar(512) NULL,
-	weight float8 NULL,	
-	CONSTRAINT products_pkey PRIMARY KEY (id)
+	weight float8 NULL
 );
+
+
 
 SELECT  *FROM public.products
